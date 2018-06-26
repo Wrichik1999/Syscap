7283f99a5bba88af38fc0ec34eb28840cf60f837
Wrichik Biswas <wrichikbiswas1999@gmail.com>
2018-06-26 22:35:15 +0100

    first

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.control.Tab;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+	
+	}
+	
+	Tab charsets() {
+		
+		return new Tab();
+	}
+}
